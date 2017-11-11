815a8fc76fe48f97e9e708e692d206ea3b664fe3
Andy Dayton <amdayton@gmail.com>
2017-11-11 12:46:16 -0600

    Still a wip

--- README.rst
+++ README.rst
@@ -5,6 +5,30 @@ A cli tool for my shitty Alcatel A392CC flip phone.
 
 Based on the the CLI tutorial here: https://stormpath.com/blog/building-simple-cli-interfaces-in-python
 
+Usage
+-----
+
+Desired functions:
+
+- Sync
+  - Sync from phone to local cache for modification
+  - sync back to phone once operations are complete
+  - Backup phone contents
+- music
+  - Convert non-mp3 music files to mp3
+  - cleanup non-mp3 music files
+  - re-write music metadata to format more useful for phone
+  - 
+- podcast
+  - some way to use podcasts on phone?
+- ebooks
+  - is there any point to using this functionality?
+- notes
+  - maybe sync latest notes from NOTES folder?
+
+
+
+
 Setup
 ------
 
--- dumbp/cli.py
+++ dumbp/cli.py
@@ -1,8 +1,40 @@
-import click
+"""
+dumbp
 
-@click.command()
-def hello():
-  click.echo('Hello world!')
+Usage:
+  dumbp
 
-if __name__ == '__main__':
-  hello()
+Options:
+  -h --help                         Show this screen.
+  --version                         Show version.
+
+Examples:
+  dumbp
+
+Help:
+  For help using this tool, please open an issue on the Github repository:
+  https://github.com/andyinabox/dumbp-a392cc-cli
+"""
+
+
+from inspect import getmembers, isclass
+
+from docopt import docopt
+
+from . import __version__ as VERSION
+
+
+def main():
+    """Main CLI entrypoint."""
+    import dumbp.commands
+    options = docopt(__doc__, version=VERSION)
+
+    # Here we'll try to dynamically match the command the user is trying to run
+    # with a pre-defined command class we've already created.
+    for (k, v) in options.items(): 
+        if hasattr(dumbp.commands, k) and v:
+            module = getattr(dumbp.commands, k)
+            dumbp.commands = getmembers(module, isclass)
+            command = [command[1] for command in dumbp.commands if command[0] != 'Base'][0]
+            command = command(options)
+command.run()
